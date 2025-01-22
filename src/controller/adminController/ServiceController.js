3e6edb1f5f095bd3f49dd46e44f85aacf50cccee
nhmnazmul22 <najmulnonir12345@gmail.com>
2025-01-22 21:32:42 +0600

    Admin Routes ---> Service Controller Completed

--- src/controller/adminController/ServiceController.js
+++ src/controller/adminController/ServiceController.js
@@ -1,1 +1,34 @@
 // ==== Internal Imports ====
+import * as ServicesService from "../../services/adminServices/ServicesService.js"
+
+// ==== Create Services Controller =====
+export const  createService = async (req, res) => {
+  const result = await ServicesService.serviceCreateService(req);
+  return res.json(result);
+};
+
+
+// ==== Update Services Controller =====
+export const  updateService = async (req, res) => {
+  const result = await ServicesService.serviceUpdateService(req);
+  return res.json(result);
+};
+
+
+// ==== Read Services Controller =====
+export const readServices = async (req, res) => {
+  const result = await ServicesService.servicesReadServicer(req);
+  return res.json(result);
+};
+
+// ==== Read Service Controller =====
+export const readService = async (req, res) => {
+  const result = await ServicesService.serviceReadService(req);
+  return res.json(result);
+};
+
+// ==== Remove Service Controller =====
+export const removeService = async (req, res) => {
+  const result = await ServicesService.serviceRemoveService(req);
+  return res.json(result);
+};
